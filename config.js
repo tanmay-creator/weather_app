c213a58ce2cf7132a9f53f97f5cc49daeb1165ea
tanmay-creator <tanmaykharkar29@gmail.com>
2020-12-20 22:51:27 +0530

    nodejs project first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "http://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "ebafdf95e9cbeaa4f56dd4f79ff74c13"
+    }
+}
+
+module.exports = constants;
